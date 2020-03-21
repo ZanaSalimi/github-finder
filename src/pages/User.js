1d9f805e37b568cf3a2e0402457f7acc4e22d285
Zana Salmi <zanasalimi21@gmail.com>
2020-03-21 15:49:49 +0430

    made Single User Component

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
-
+import { MapPin, Mail, Link, Code } from 'react-feather'
 export class User extends Component {
     render() {
         const { login, avatar_url, name, bio, location, email, blog, hireable } = this.props.username;
         return (
-            <div>
-                {login}
+            <div className="container mt-5">
+                <div className="header-box row px-3 py-4">
+                    <div className="biography col-8 d-flex">
+                        <img src={ avatar_url } alt={ name } className="rounded-circle profile-img mr-5" /> 
+                        <div className="bio py-2">
+                            <h2>{ login }</h2>
+                            <h3>{ name }</h3>
+                            <p>{ bio }</p>
+                        </div>
+                    </div>
+                    <div className="contact col-4">
+                        <p><MapPin className="contact-ico" />  { location }</p>
+                        <p><Mail className="contact-ico" />  { email }</p>
+                        <p><Link className="contact-ico" />  { blog }</p>
+                        <p><Code className="contact-ico" />  Hirable: { hireable ? 'Available' : 'Unavailable' }</p>
+                    </div>
+                </div>
             </div>
         )
     }
